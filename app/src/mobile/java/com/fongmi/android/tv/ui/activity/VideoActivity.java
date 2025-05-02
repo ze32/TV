c9ecd774cc58d475b329f2666d301a45ca9ce10b
z <>
2025-05-02 14:13:29 +0800

    Resolve the issue preventing automatic source switching for non-episodic video content

--- app/src/mobile/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
+++ app/src/mobile/java/com/fongmi/android/tv/ui/activity/VideoActivity.java
@@ -1008,7 +1008,10 @@ public class VideoActivity extends BaseActivity implements Clock.Callback, Custo
         boolean empty = item.getVodFlags().isEmpty();
         mBinding.flag.setVisibility(empty ? View.GONE : View.VISIBLE);
         if (empty) {
-            ErrorEvent.flag(tag);
+            // ErrorEvent.flag(tag);
+            // ---------------- 修影片没有剧集时无法自动换源的bug start -----------------
+            onError();
+            // ---------------- 修影片没有剧集时无法自动换源的bug end -----------------
         } else {
             onItemClick(mHistory.getFlag());
             if (mHistory.isRevSort()) reverseEpisode(true);
@@ -1205,6 +1208,17 @@ public class VideoActivity extends BaseActivity implements Clock.Callback, Custo
         mBinding.swipeLayout.setEnabled(true);
         Track.delete(mPlayers.getUrl());
         showError(event.getMsg());
+        mClock.setCallback(null);
+        mPlayers.resetTrack();
+        mPlayers.reset();
+        mPlayers.stop();
+        startFlow();
+    }
+
+    private void onError() {
+        mBinding.swipeLayout.setEnabled(true);
+        Track.delete(mPlayers.getUrl());
+        showError(ResUtil.getString(R.string.error_play_flag));
         mClock.setCallback(null);
         mPlayers.resetTrack();
         mPlayers.reset();
